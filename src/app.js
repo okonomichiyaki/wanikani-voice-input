b461bb6e39967588e374a365e6bda05c5de0048a
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2023-04-11 20:56:12 -0400

    initial code for icon

--- src/app.js
+++ src/app.js
@@ -71,5 +71,38 @@ function main() {
   state = "Ready";
 };
 
+function setup() {
+  const menu = document.querySelector('div.character-header__menu-navigation');
+  const item = document.createElement('div');
+  item.classList.add('character-header__menu-navigation-link');
+  item.style = 'margin-left: 10px;';
+  const link = document.createElement('a');
+  link.title = 'Toggle voice input';
+  link.classList.add('summary-button');
+
+  let state = true;
+  link.onclick = function(e) {
+    console.log(`[wanikani-voice-input] >> mic click`);
+    if (state) {
+      icon.classList.remove('fa-solid');
+      icon.classList.add('fa-light');
+    } else {
+      icon.classList.remove('fa-solid');
+      icon.classList.add('fa-light');
+    }
+    state = !state;
+  };
+
+  const icon = document.createElement('i');
+  icon.classList.add('wk-icon');
+  icon.classList.add('fa-solid');
+  icon.classList.add('fa-microphone');
+
+  link.appendChild(icon);
+  item.appendChild(link);
+  menu.appendChild(item);
+}
+
 wk.checkDom();
 main();
+setup();
