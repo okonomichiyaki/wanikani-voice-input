483e9b83636e82c810bdb7e09a4167c8b6901de6
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2023-10-11 18:23:18 -0400

    version 0.4-rc1

--- dist/wanikani-voice-input.user.js
+++ dist/wanikani-voice-input.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         wanikani voice input
 // @namespace    net.cbgpnck.wkvi
-// @version      0.3.2
+// @version      0.4-rc1
 // @description  Enables doing WaniKani reviews and lessons via voice input using Web Speech API
 // @author       okonomichiyaki
 // @match        https://www.wanikani.com/*
 
-// @resource     jmdict      https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/0.3.2/data/jmdict.json
-// @resource     kanjidic2   https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/0.3.2/data/kanjidic2.json
-// @require      https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/0.3.2/dist/app.js
+// @resource     jmdict      https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/0.4-rc1/data/jmdict.json
+// @resource     kanjidic2   https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/0.4-rc1/data/kanjidic2.json
+// @require      https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/0.4-rc1/dist/app.js
 
 // @grant        GM_getResourceText
 // @grant        unsafeWindow
