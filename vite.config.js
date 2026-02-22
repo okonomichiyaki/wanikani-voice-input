fdd5859a9a695fcdcd24b6e2b6aeaa7bb4a7ddaf
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2026-02-22 17:02:06 -0500

    add license header

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import license from 'rollup-plugin-license';
 
 export default defineConfig({
   test: {
@@ -14,5 +15,47 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: false,
     minify: false,
+    rollupOptions: {
+      plugins: [
+        (() => {
+          let deps = [];
+          const inner = license({
+            thirdParty: {
+              includePrivate: false,
+              output: (dependencies) => {
+                deps = dependencies;
+              },
+            },
+          });
+          return {
+            name: 'license-banner',
+            renderChunk: inner.renderChunk,
+            generateBundle(options, bundle) {
+              inner.generateBundle.call(this, options, bundle);
+              if (deps.length === 0) return;
+              const lines = ['/*!', ' * Bundled third-party licenses:', ' *'];
+              for (const dep of deps) {
+                const author = dep.author && dep.author.name ? ` by ${dep.author.name}` : '';
+                lines.push(` * @${dep.name} v${dep.version}${author}`);
+                lines.push(` * License: ${dep.license}`);
+                if (dep.licenseText) {
+                  for (const line of dep.licenseText.split('\n')) {
+                    lines.push(` * ${line}`);
+                  }
+                }
+                lines.push(' *');
+              }
+              lines.push(' */');
+              const banner = lines.join('\n') + '\n';
+              for (const chunk of Object.values(bundle)) {
+                if (chunk.type === 'chunk') {
+                  chunk.code = banner + chunk.code;
+                }
+              }
+            },
+          };
+        })(),
+      ],
+    },
   },
 });
