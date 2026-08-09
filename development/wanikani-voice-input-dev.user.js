a0581001e1f281755c5f9155c34167a9495c952d
okonomichiyaki <74801510+okonomichiyaki@users.noreply.github.com>
2026-08-09 12:21:35 -0400

    add tampermonkey script for local dev

---
+++ development/wanikani-voice-input-dev.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         wanikani voice input (dev)
+// @namespace    net.cbgpnck.wkvi
+// @version      0.0.0
+// @description  Enables doing WaniKani reviews and lessons via voice input using Web Speech API
+// @author       okonomichiyaki
+// @match        https://www.wanikani.com/*
+
+// @resource     jmdict      https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/latest/data/jmdict.json
+// @resource     kanjidic2   https://raw.githubusercontent.com/okonomichiyaki/wanikani-voice-input/latest/data/kanjidic2.json
+// @require      file:///Users/aki/Code/wanikani-voice-input/dist/app.js
+
+// @grant        GM_getResourceText
+// @grant        unsafeWindow
+// @run-at       document-end
+// ==/UserScript==
